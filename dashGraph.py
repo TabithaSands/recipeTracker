6707c37f75aae26dd8769eb37c8b8b858c762d4c
tabitha <Tabitha.sands11@gmail.com>
2024-02-28 21:27:19 -0600

    wip

--- dashGraph.py
+++ dashGraph.py
@@ -8,7 +8,7 @@ app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(df.year.unique(), '2000', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
 
@@ -17,8 +17,16 @@ app.layout = html.Div([
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country == value]
-    return px.bar(dff, x='year', y='pop')
+    # dff = df[df.country == value]
+    d1 = df["continent"]
+    d2 = df["country"]
+    d3 = df[df["year"] == value]
+    d4 = d3["pop"]
+    data = dict(
+        c = d1
+    )
+    return px.sunburst(data)
+    # return px.bar(dff, x='year', y='pop')
     # return px.line(dff, x='year', y='pop')
 
 
