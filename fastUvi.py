d819e5774c360ea5120d55998610e01debcbb823
tabitha <Tabitha.sands11@gmail.com>
2024-03-02 13:48:13 -0600

    update

--- fastUvi.py
+++ fastUvi.py
@@ -1,8 +1,6 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
 from allrecipes import AllRecipes
 from fastapi.templating import Jinja2Templates
-
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates/')
@@ -13,12 +11,24 @@ def find_recipe(search_item):
     query_result = AllRecipes.search(search_item)
 
     # Get:
-    main_recipe_url = query_result[0]['url']
-    detailed_recipe = AllRecipes.get(main_recipe_url)
-    return detailed_recipe
+    # main_recipe_url = query_result[0]['url']
+    # detailed_recipe = AllRecipes.get(main_recipe_url)
+    return query_result
 
 
 @app.get("/")
 def read_root():
     detailed_recipe = find_recipe("mac and cheese")
     return templates.TemplateResponse('home.html', context={'request': detailed_recipe, 'recipe': detailed_recipe})
+
+
+@app.get("/form")
+def form_post(request: Request):
+    result = "Search a recipe"
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+
+
+@app.post("/form")
+def form_post(request: Request, search_item: str = Form(...)):
+    result = find_recipe(search_item)
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
